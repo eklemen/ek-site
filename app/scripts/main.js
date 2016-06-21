c5aca4469562cb760a9c4a189f377b65d49e2220
Evan Klemen <evan@Janes-MacBook-Pro.local>
2016-06-21 14:36:56 -0400

    adding back scrollspy

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,5 +3,11 @@ $(function() {
         offset: 50,
         updateURL: true,
     });
-
+	$('.hamburger-menu').on('click', function() {
+		$('.bar').toggleClass('animate');
+	})
+	$('.nav a').on('click', function(){
+		$('.navbar-toggle').click();
+		$('.bar').toggleClass('animate');
+	})
 });
