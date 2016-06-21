477517bea939bd7d31ca2c14d718ecfaf23e4dad
Evan Klemen <evan@Janes-MacBook-Pro.local>
2016-06-21 17:51:00 -0400

    added contact form

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -10,4 +10,35 @@ $(function() {
 		$('.navbar-toggle').click();
 		$('.bar').toggleClass('animate');
 	})
+	var submit = function(data){
+		
+	}
+
+	$('form').attr("action", "https://formspree.io/ejklemen@gmail.com");
+	$('form').submit(function(e){
+		e.preventDefault();
+		$('form').serialize();
+		var formData = {
+            'name'  : $('input[name=name]').val(),
+            'email' : $('input[name=_replyto]').val(),
+            'body'  : $('textarea[name=body]').val()
+        };
+        $.ajax({
+            url: "https://formspree.io/ejklemen@gmail.com", 
+            method: "POST",
+            // accepts: 'application/json',
+            data: formData,
+            dataType: "json"
+        }).done(function(data){
+        	e.preventDefault();
+        	$('form')[0].reset();
+        	swal(
+        	  'Success!',
+        	  'Your message has been sent! I will get back to you shortly.',
+        	  'success'
+        	)
+        });
+        return false;
+	});
+	
 });
